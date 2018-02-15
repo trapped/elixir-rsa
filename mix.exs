d9b27299bc4490fd0359bceed7b92c37fc5e234f
Connor Rigby <connor@farmbot.io>
2018-02-15 14:01:21 -0800

    Run Elixir Formatter on the rest of the Elixir files

--- mix.exs
+++ mix.exs
@@ -2,16 +2,20 @@ defmodule RSA.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :rsa,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     package: package(),
-     description: "Erlang public_key cryptography wrapper"]
+    [
+      app: :rsa,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      package: package(),
+      description: "Erlang public_key cryptography wrapper"
+    ]
   end
 
   defp package do
-    [contributors: ["trapped"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/trapped/elixir-rsa"}]
+    [
+      contributors: ["trapped"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/trapped/elixir-rsa"}
+    ]
   end
 end
